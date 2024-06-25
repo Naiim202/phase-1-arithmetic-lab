2b3f80230998c6fa9a5dd7b2f6c09e3777f94fc8
hani <hani@DESKTOP-5I66P1U.localdomain>
2024-06-25 16:05:36 +0300

    arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+console.log(random);
+
+const num3 = 14;
+const num4 = 10;
+const mod = (num3 % num4);
+console.log(mod);
+
+const numbers = [3, 8, 12, 20, 4];
+const max = Math.max(...numbers);
+console.log(max);
